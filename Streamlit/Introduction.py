b5fd70bb8239f0f8be844f1d244c62918f0ba43d
kirakurostar <bahiaspcl@gmail.com>
2024-12-15 21:47:37 +0000

    fixing the intoduction title

--- Streamlit/Introduction.py
+++ Streamlit/Introduction.py
@@ -4,25 +4,13 @@
 import streamlit as st
 
 # Title of the presentation with custom dark mode styling
-st.markdown(
-    """
-    <style>
-        .title {
-            font-size: 40px;
-            font-weight: bold;
-            color: #11111;
-            text-align: left;
-            margin-top: 20px;
-            margin-bottom: 20px;
-        }
-    </style>
-    """, unsafe_allow_html=True)
+
 
 # Title with a centered style
-st.markdown('<p class="title">Web App Frameworks: An Introduction to Streamlit</p>', unsafe_allow_html=True)
+st.write("# Web App Frameworks: An Introduction to Streamlit")
 
 # Introduction section
-st.header("Introduction")
+st.write("## Introduction")
 st.write(""" 
 Welcome to this presentation on web app frameworks, with a special focus on Streamlit. 
 By the end of this session, you will understand what web app frameworks are, their general uses and accessibility, 
